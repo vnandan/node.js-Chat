0bcd92733ae272c03f7b143ef8a19363a23bfbea
Vipul Nandan <vipnandan@gmail.com>
2014-08-17 03:22:47 +0530

    messages passing in console.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ client = require('socket.io').listen(8080).sockets;
 client.on('connection',function(socket){
 	socket.on('input',function(data){
 		console.log(data);
+		socket.emit('output',data['message']);
+		client.emit('output',data['message']);
 	});
 	console.log('new connection');
 });
